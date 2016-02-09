b0570c57dde29e25d653ca1595268e48ffd06b2a
Stephan Heilner <stephanheilner@gmail.com>
2016-02-09 15:07:52 -0700

    Initial Commit

---
+++ TestRunner/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestRunner
+//
+//  Created by Stephan Heilner on 2/9/16.
+//  Copyright © 2016 The Church of Jesus Christ of Latter-day Saints. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
